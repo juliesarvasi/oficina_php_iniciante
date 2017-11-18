a55d1b83a98263c752e5d2327fe95c76d39b3a0a
julie_costa_ <julie_costa_@hotmail.com>
2017-11-18 14:00:30 -0500

    meu primeiro commit

--- index.php
+++ index.php
@@ -8,9 +8,138 @@
     <body>
                         
         <?php
+      ini_set('display_errors', 1);
 
-            // Coloque seu código aqui!
-
+           /*$cor = "amarelo";  //nome de uma cor
+           $cor = "vermelho"; //onutra variavel
+           echo $cor; //chamar a cor
+      
+          var_dump(array(32,"julie")). "<br>";
+          var_dump("julie"). "<br>";
+          
+          $a = "Olá";
+          $b = "mundo";
+          echo $a ." ". $b;
+      
+          $nome = "Julie";
+          echo "Olá " . $nome . "!";
+      
+          $a = 3;
+          $b = 5;
+      
+          $resultado =  $a % $b;
+      
+          echo $resultado
+      
+          $a = 5;
+          $b = 2;
+      
+          if ($a > $b) {
+            echo "a é maior que b";
+          } else if ($a < $b) {
+              echo " b é maior que a";
+          } else {
+              echo "a e b são iguais";     
+          }
+      
+          $a = 1;
+          $b = "1";
+      
+          if ($a == $b) { //verifica se valores são iguaiis independente do tipo string,integer.
+            echo " a igual a b";
+          } else {
+            " a diferente de b";
+          }
+ 
+          $i = 1;
+          
+          while ($i < 10) {
+            $i = $i + 1;
+            echo $i. "<br>";
+            
+            echo "o valor de $i é par<br>" // não precisa concatenar quando temos aspas duplo
+          }
+      
+          $i = 1;
+      
+          while ($i < 10) {
+            $i = $i + 1;
+            if ($i % 2 == 0) {
+              echo $i . " é número par". "<br>";
+            } else {
+              echo $i . " é número ímpar". "<br>";
+            }
+          }
+      
+          for($i = 1; $i < 10; $i++) {
+            echo "O valor de i é $i<br>";
+          } 
+            
+          for($i = 1; $i <=10; $i++) {
+            if($i % 2 == 0) {
+              echo $i . " é número par". "<br>";
+            } else {
+              echo $i . " é número ímpar". "<br>";
+            }
+          } 
+      
+          $nome = "Julie";
+      
+          switch ($nome) {
+            case "Julie":
+                echo "Bem vinda Julie!";
+              break;
+            case "Julia":
+                echo "Bem vinda Julia!";
+              break;
+            default:
+              echo "Bem vindo(a)!";
+          }
+        
+          function soma($a, $b) {
+            $resultado = $a + $b;
+              return $resultado;
+          }
+      
+      $numero1 = 10.2;
+      $numero2 = 6;
+      
+      $resultfuncao = soma($numero1, $numero2);
+      
+      echo $resultfuncao;
+      
+        $cores = array("vermelho", "azul"); // ou $cores = ["vermelho", "azul"];
+      
+        echo $cores[1];  //contagem de arrays começa do 0
+      
+        $pessoas = ["nome" => "Ana", "idade" => 23];
+        echo $pessoas["nome"] . " " . $pessoas["idade"];
+      
+        $numeros = [1, 2, 3, 4];
+        array_push($numeros, 5);
+        echo $numeros[4]; 
+      
+        $frutas = array("banana", "laranja", "melancia");
+       
+        foreach($frutas as $fruta) {
+          echo "A fruta é $fruta<br>";
+        }
+      
+        echo "<br>adicionei abacaxi<br><br>";
+      
+        array_push($frutas, "abacaxi");
+        foreach($frutas as $fruta) {
+          echo "A fruta é $fruta<br>";
+        }
+      
+        echo "<br>retirei abacaxi<br><br>";
+      
+        array_pop($frutas);
+        foreach($frutas as $fruta) {
+          echo "A fruta é $fruta<br>";
+        }*/
+        
+         
         ?>
 
     </body>
